c7117aaac233416ae9d70cab57ac996f9b28aa2f
Gregor Burger <burger.gregor@gmail.com>
2009-02-16 17:59:05 +0100

    implement fileout node

--- nodes/fileout.cpp
+++ nodes/fileout.cpp
@@ -1,6 +1,9 @@
 #include "fileout.h"
 
 #include <flow.h>
+#include <QFile>
+#include <QTextStream>
+#include <boost/foreach.hpp>
 
 FileOut::FileOut()
 	: Node()
@@ -10,10 +13,28 @@ FileOut::FileOut()
 }
 
 FileOut::~FileOut() {
+	delete file;
+}
 
+void FileOut::init(int start, int stop, int dt) {
+	(void) start;
+	(void) stop;
+	(void) dt;
+	assert(out_file_name);
+	file = new QFile(out_file_name->c_str());
+	stream = new QTextStream(file);
+}
+
+void FileOut::deinit() {
+	file->close();
 }
 
 void FileOut::f(int time, int dt) {
 	(void) time;
 	(void) dt;
+	(*stream) << time;
+	for (int i = 0; i < in->getSize(); i++) {
+		(*stream) << "\t" << in->const_data[i];
+	}
+	(*stream) << endl;
 }
--- nodes/fileout.h
+++ nodes/fileout.h
@@ -2,16 +2,21 @@
 #define FILEOUT_H
 
 #include <node.h>
+class QFile;
+class QTextStream;
 
 CD3_DECLARE_NODE(FileOut)
 public:
 	FileOut();
 	~FileOut();
 	void f(int time, int dt);
-
+	void init(int start, int stop, int dt);
+	void deinit();
 private:
 	Flow *in;
 	std::string *out_file_name;
+	QFile *file;
+	QTextStream *stream;
 };
 
 #endif // FILEOUT_H
