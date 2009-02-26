716b2c6fe265103b7a3f3a7e8bec5f624a20623d
Gregor Burger <burger.gregor@gmail.com>
2009-02-26 10:02:07 +0100

    - remove mix function which was added to the flow.h and flow.cpp files

--- nodes/mixer.cpp
+++ nodes/mixer.cpp
@@ -39,34 +39,6 @@ void Mixer::init(int start, int end, int dt) {
 	inited = true;
 }
 
-Flow *mix(Flow **inputs, int num_inputs) {
-	assert(num_inputs > 1, "cannot mix one input");
-	double qe = 0;
-	std::string qename = inputs[0]->getUnitNames(CalculationUnit::flow)[0];
-
-	Flow *f = new Flow();
-
-	for (int i = 0; i < num_inputs; i++) {
-		assert(inputs[0]->getUnitNames(CalculationUnit::flow).size() == 1,
-			   "unable to mix more than one flow");
-		qe += inputs[i]->getValue(qename);
-	}
-
-	f->addUnit(qename, CalculationUnit::flow, qe);
-
-	for (size_t c = 0;
-		 c < inputs[0]->getUnitNames(CalculationUnit::concentration).size();
-		 c++) {
-		double Ci = 0;
-		std::string cname = inputs[0]->getUnitNames(CalculationUnit::concentration).at(c);
-		for (int i = 0; i < num_inputs; i++) {
-			Ci += inputs[i]->getValue(cname);
-		}
-		f->addUnit(cname, CalculationUnit::concentration, Ci/qe);
-	}
-	return f;
-}
-
 void Mixer::f(int time, int dt) {
 	(void) time;
 	(void) dt;
