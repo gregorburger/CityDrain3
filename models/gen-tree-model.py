a47d053a1b9c63ac6e351973c57beaa9ba159874
Gregor Burger <gregor@gregor-laptop.(none)>
2009-04-14 10:09:29 +0000

    implement tree model generation

--- models/gen-tree-model.py
+++ models/gen-tree-model.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python
 from cd3modelgen import *
+import sys
 
-
-c1=ConstSource()
-c2=ConstSource()
-
-s1=Sewer()
-s2=Sewer()
-
-m1=Mixer()
-
-f=FileOut("tmp/test-gen-out.txt")
-
+max_nesting = 10
 s=Simulation()
-s.nodes+=[s1, s2, c1, c2, m1, f]
-s.cons+=[Connection(c1, s1), \
-	Connection(c2, s2), \
-	Connection(c1, m1, "out", "inputs[0]"), \
-	Connection(c2, m1, "out", "inputs[1]"), \
-	Connection(m1, f)]
 
-s.render()
+def to_tree(parent_node, nesting = 0):
+    if nesting == max_nesting:
+        c=ConstSource()
+        s.nodes += [c]
+        s.cons+=[Connection(c, parent_node)]
+    else:
+        m = Mixer()
+        s1 = Sewer()
+        s2 = Sewer()
+        s.nodes+=[m, s1, s2]
+        s.cons+=[Connection(m, parent_node),
+                 Connection(s1, m, "out", "inputs[0]"),
+                 Connection(s2, m, "out", "inputs[1]")]
+        to_tree(s1, nesting+1)
+        to_tree(s2, nesting+1)
 
+def main():
+    f = FileOut("tmp/tree-out.txt")
+    to_tree(f)
+    s.nodes += [f]
+    s.render()
+    pass
+
+if __name__ == "__main__":
+    main()
