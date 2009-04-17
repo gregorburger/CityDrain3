6730129a26dd098a8550a5ecc514b541e6d0cfab
Gregor Burger <gregor@gregor-laptop.(none)>
2009-04-17 12:05:58 +0200

    make it compile again on linux

--- cd3core/cd3globals.h
+++ cd3core/cd3globals.h
@@ -4,8 +4,11 @@
 #ifdef _MSC_VER
 #pragma warning( disable: 4251 )
 #endif
-
+#ifdef _WIN32
 //#define CD3_PUBLIC __attribute__ ((visibility("default")))
 #define CD3_PUBLIC __declspec(dllexport)
+#else
+#define CD3_PUBLIC
+#endif
 
 #endif // CD3GLOBALS_H
