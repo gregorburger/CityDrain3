9830bcdaa707cd4764fd2d80b5a98e7b3f81f9b8
Gregor Burger <gregor@gregor-laptop.(none)>
2009-04-18 03:00:20 +0200

    non recursive mode gen

--- models/gen-long-sewer.py
+++ models/gen-long-sewer.py
@@ -3,22 +3,22 @@ import sys
 
 s = Simulation("PipelinedSimulation")
 
-def gensewer(sink, c):
-	source = Sewer()
-	s.nodes += [source]
-	s.cons += [Connection(source, sink)]
-	if (c != 0):
-		gensewer(source, c - 1)
-	else:
-		c = ConstSource()
-		s.nodes += [c]
-		s.cons += [Connection(c, source)]
-
-def main():
+def gensewer(count):
+	c = ConstSource()
+	s.nodes += [c]
+	old = c
+	for i in xrange(count):
+		sink = Sewer()
+		s.nodes += [sink]
+		s.cons += [Connection(old, sink)]
+		old = sink
 	f = FileOut("tmp/longsewerout.txt")
-	gensewer(f, 100)
 	s.nodes += [f]
+	s.cons += [Connection(old, f)]
+	
+def main():
+	gensewer(5000)
 	s.render()
-
+	
 if __name__ == "__main__":
 	main()
