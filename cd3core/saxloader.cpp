5575b4410f9a53cd780923a33a21dc7afa4fecbf
Gregor Burger <burger.gregor@gmail.com>
2009-04-21 13:38:21 +0200

    spaces to tab fix flow loading

--- cd3core/saxloader.cpp
+++ cd3core/saxloader.cpp
@@ -18,211 +18,216 @@ using namespace boost;
 #include <calculationunit.h>
 
 struct SaxLoaderPriv {
-    NodeRegistry node_registry;
-    SimulationRegistry sim_registry;
-    TypeRegistry type_registry;
-    IModel *model;
-    ISimulation *simulation;
-    Flow f;
-    std::string param_name;
+	NodeRegistry node_registry;
+	SimulationRegistry sim_registry;
+	TypeRegistry type_registry;
+	IModel *model;
+	ISimulation *simulation;
+	Flow f;
+	std::string param_name;
 };
 
 SaxLoader::SaxLoader(IModel *model) {
-    pd = new SaxLoaderPriv();
-    pd->model = model;
-    pd->simulation = 0;
+	pd = new SaxLoaderPriv();
+	pd->model = model;
+	pd->simulation = 0;
 }
 
 SaxLoader::~SaxLoader() {
-    delete pd;
+	delete pd;
 }
 
 bool SaxLoader::startElement(const QString &/*ns*/,
-                      const QString &lname,
-                      const QString &/*qname*/,
-                      const QXmlAttributes &atts) {
-    if (lname == "node") {
-        std::string id = atts.value("id").toStdString();
-        std::string klass = atts.value("class").toStdString();
+							 const QString &lname,
+							 const QString &/*qname*/,
+							 const QXmlAttributes &atts) {
+	if (lname == "node") {
+		std::string id = atts.value("id").toStdString();
+		std::string klass = atts.value("class").toStdString();
 
-        current = pd->node_registry.createNode(klass);
-        pd->model->addNode(id, current);
-        return true;
-    }
-    if (lname == "sink") {
-        sink_id = atts.value("node").toStdString();
-        sink_port = atts.value("port").toStdString();
-        return true;
-    }
-    if (lname == "source") {
-        source_id = atts.value("node").toStdString();
-        source_port = atts.value("port").toStdString();
-        return true;
-    }
-    if (lname == "parameter") {
-        loadParameter(atts);
-        return true;
-    }
-    if (lname == "simulation") {
-        cd3assert(pd->simulation == 0, "Simulation already set");
-        pd->simulation = pd->sim_registry.createSimulation(atts.value("class").toStdString());
-        return true;
-    }
-    if (lname == "time") {
-        SimulationParameters p;
-        p.dt = lexical_cast<int>(atts.value("dt").toStdString());
-        p.start = lexical_cast<int>(atts.value("start").toStdString());
-        p.stop = lexical_cast<int>(atts.value("stop").toStdString());
-        pd->simulation->setSimulationParameters(p);
-        return true;
-    }
-    typedef CalculationUnit CU;
-    if (lname == "unit") {
-        std::string kind = atts.value("kind").toStdString();
-        CU *cunit = CU::fromString(kind);
-        cd3assert(cunit != CU::null,
-                  str(format("can't determine flow unit type: [%1%]") % kind));
-        double value = lexical_cast<double>(atts.value("value").toStdString());
-        pd->f.addUnit(atts.value("name").toStdString(),
-                      cunit,
-                      value);
-        return true;
-    }
-    if (lname == "pluginpath") {
-        loadPlugin(atts.value("path"));
-        return true;
-    }
-    if (lname == "citydrain") {
-        return true;
-    }
-    if (lname == "model") {
-        return true;
-    }
-    if (lname == "nodelist") {
-        return true;
-    }
-    if (lname == "connectionlist") {
-        return true;
-    }
-    if (lname == "flow") {
-        pd->f = Flow::nullFlow();
-        return true;
-    }
-    if (lname == "connection") {
-        return true;
-    }
-    qWarning() << "not used element " << lname;
-    return true;
+		current = pd->node_registry.createNode(klass);
+		pd->model->addNode(id, current);
+		return true;
+	}
+	if (lname == "sink") {
+		sink_id = atts.value("node").toStdString();
+		sink_port = atts.value("port").toStdString();
+		return true;
+	}
+	if (lname == "source") {
+		source_id = atts.value("node").toStdString();
+		source_port = atts.value("port").toStdString();
+		return true;
+	}
+	if (lname == "parameter") {
+		loadParameter(atts);
+		return true;
+	}
+	if (lname == "simulation") {
+		cd3assert(pd->simulation == 0, "Simulation already set");
+		std::string klass = atts.value("class").toStdString();
+		pd->simulation = pd->sim_registry.createSimulation(klass);
+		return true;
+	}
+	if (lname == "time") {
+		SimulationParameters p;
+		p.dt = lexical_cast<int>(atts.value("dt").toStdString());
+		p.start = lexical_cast<int>(atts.value("start").toStdString());
+		p.stop = lexical_cast<int>(atts.value("stop").toStdString());
+		pd->simulation->setSimulationParameters(p);
+		return true;
+	}
+	typedef CalculationUnit CU;
+	if (lname == "unit") {
+		std::string kind = atts.value("kind").toStdString();
+		CU *cunit = CU::fromString(kind);
+		cd3assert(cunit != CU::null,
+				  str(format("can't determine flow unit type: [%1%]") % kind));
+		double value = lexical_cast<double>(atts.value("value").toStdString());
+		std::cout << format("unit %1% has value %2%") % atts.value("name").toStdString() % value << std::endl;
+		pd->f.addUnit(atts.value("name").toStdString(),
+					  cunit,
+					  value);
+		return true;
+	}
+	if (lname == "pluginpath") {
+		loadPlugin(atts.value("path"));
+		return true;
+	}
+	if (lname == "citydrain") {
+		return true;
+	}
+	if (lname == "model") {
+		return true;
+	}
+	if (lname == "nodelist") {
+		return true;
+	}
+	if (lname == "connectionlist") {
+		return true;
+	}
+	if (lname == "flow") {
+		//pd->f = Flow::nullFlow();
+		pd->f = Flow();
+		return true;
+	}
+	if (lname == "connection") {
+		return true;
+	}
+	qWarning() << "not used element " << lname;
+	return true;
 }
 
 ISimulation *SaxLoader::load(QFile &file) {
-    bool opened = file.open(QIODevice::ReadOnly);
-    cd3assert(opened, "could not open model");
-    QXmlSimpleReader r;
-    r.setContentHandler(this);
-    QXmlInputSource source(&file);
-    r.parse(&source);
-    return pd->simulation;
+	bool opened = file.open(QIODevice::ReadOnly);
+	cd3assert(opened, "could not open model");
+	QXmlSimpleReader r;
+	r.setContentHandler(this);
+	QXmlInputSource source(&file);
+	r.parse(&source);
+	return pd->simulation;
 }
 
 bool SaxLoader::endElement(const QString &/*ns*/,
-                    const QString &lname,
-                    const QString &/*qname*/) {
+						   const QString &lname,
+						   const QString &/*qname*/) {
 
-    if (lname == "connection") {
-        cd3assert(!source_id.empty(), "source node not set");
-        cd3assert(!source_port.empty(), "source port not set");
-        cd3assert(!sink_id.empty(), "sink node not set");
-        cd3assert(!sink_port.empty(), "sink port not set");
-        pd->model->addConnection(source_id,
-                                 source_port,
-                                 sink_id,
-                                 sink_port);
-    }
-    if (lname == "nodelist") {
-        pd->model->initNodes(pd->simulation->getSimulationParameters());
-    }
-    if (lname == "flow") {
-        current->setParameter<Flow>(pd->param_name, pd->f);
-    }
-    return true;
+	if (lname == "connection") {
+		cd3assert(!source_id.empty(), "source node not set");
+		cd3assert(!source_port.empty(), "source port not set");
+		cd3assert(!sink_id.empty(), "sink node not set");
+		cd3assert(!sink_port.empty(), "sink port not set");
+		pd->model->addConnection(source_id,
+								 source_port,
+								 sink_id,
+								 sink_port);
+	}
+	if (lname == "nodelist") {
+		qDebug() << "initing nodes";
+		pd->model->initNodes(pd->simulation->getSimulationParameters());
+	}
+	if (lname == "flow") {
+		current->setParameter<Flow>(pd->param_name, pd->f);
+	}
+	return true;
 }
 
 void SaxLoader::loadPlugin(QString path) {
-    QLibrary l(path);
-    bool loaded = l.load();
-    cd3assert(loaded, str(format("could not load plugin %1%") % path.toStdString()));
-    regTypeFunProto regTypeFun = (regTypeFunProto) l.resolve("registerTypes");
-    if (regTypeFun) {
-            regTypeFun(&pd->type_registry);
-    } else {
-            qWarning() << path << " has no type register hook";
-    }
-    regSimFunProto regSimFun = (regSimFunProto) l.resolve("registerSimulations");
-    if (regSimFun) {
-            regSimFun(&pd->sim_registry);
-    } else {
-            qWarning() << path << " has no node register hook";
-    }
-    regNodeFunProto regNodeFun = (regNodeFunProto) l.resolve("registerNodes");
-    if (regNodeFun) {
-            regNodeFun(&pd->node_registry);
-    } else {
-            qWarning() << path << " has no node register hook";
-    }
+	QLibrary l(path);
+	bool loaded = l.load();
+	cd3assert(loaded, str(format("could not load plugin %1%") % path.toStdString()));
+	regTypeFunProto regTypeFun = (regTypeFunProto) l.resolve("registerTypes");
+	if (regTypeFun) {
+		regTypeFun(&pd->type_registry);
+	} else {
+		qWarning() << path << " has no type register hook";
+	}
+	regSimFunProto regSimFun = (regSimFunProto) l.resolve("registerSimulations");
+	if (regSimFun) {
+		regSimFun(&pd->sim_registry);
+	} else {
+		qWarning() << path << " has no node register hook";
+	}
+	regNodeFunProto regNodeFun = (regNodeFunProto) l.resolve("registerNodes");
+	if (regNodeFun) {
+		regNodeFun(&pd->node_registry);
+	} else {
+		qWarning() << path << " has no node register hook";
+	}
 }
 
 void SaxLoader::loadParameter(const QXmlAttributes& atts) {
-    bool simple = atts.value("kind") == "simple";
+	bool simple = atts.value("kind") != "complex";
 
-    std::string name = atts.value("name").toStdString();
-    std::string type = "double";
-    if (atts.index("type") > 0)
-        type = atts.value("type").toStdString();
+	std::string name = atts.value("name").toStdString();
+	std::cout << "loading paramter " <<  name << std::endl;
+	std::string type = "double";
+	if (atts.index("type") > 0)
+		type = atts.value("type").toStdString();
 
-    cd3assert(current->const_parameters->count(name),
-              str(format("no such parameter in node %1%") % name.c_str()));
+	cd3assert(current->const_parameters->count(name),
+			  str(format("no such parameter in node %1%") % name.c_str()));
 
-    if (simple) {
-        QString value = atts.value("value");
+	if (simple) {
+		QString value = atts.value("value");
 
-        if (type == "double") {
-                double d = value.toDouble();
-                current->setParameter<double>(name, d);
-                return;
-        }
+		if (type == "double") {
+			double d = value.toDouble();
+			current->setParameter<double>(name, d);
+			return;
+		}
 
-        if (type == "float") {
-                float d = value.toFloat();
-                current->setParameter<float>(name, d);
-                return;
-        }
+		if (type == "float") {
+			float d = value.toFloat();
+			current->setParameter<float>(name, d);
+			return;
+		}
 
-        if (type == "int") {
-                int ivalue = value.toInt();
-                current->setParameter<int>(name, ivalue);
-                return;
-        }
+		if (type == "int") {
+			int ivalue = value.toInt();
+			current->setParameter<int>(name, ivalue);
+			return;
+		}
 
-        if (type == "bool") {
-                bool bvalue = value == "true";
-                current->setParameter<bool>(name, bvalue);
-                return;
-        }
+		if (type == "bool") {
+			bool bvalue = value == "true";
+			current->setParameter<bool>(name, bvalue);
+			return;
+		}
 
-        if (type == "string") {
-                std::string std = value.toStdString();
-                current->setParameter<std::string>(name, std);
-                return;
-        }
+		if (type == "string") {
+			std::string std = value.toStdString();
+			current->setParameter<std::string>(name, std);
+			return;
+		}
 
-        //xmlError(element, QString().sprintf("%s simple type unknown", type.c_str()));
-    } else {
-        if (type == "Flow") {
-            pd->param_name = atts.value("name").toStdString();
-            //pd->f.clear();
-        }
-        return;
-    }
-    qWarning() << "unknown type " << atts.value("type");
+		//xmlError(element, QString().sprintf("%s simple type unknown", type.c_str()));
+	} else {
+		if (type == "Flow") {
+			pd->param_name = atts.value("name").toStdString();
+			//pd->f.clear();
+		}
+		return;
+	}
+	qWarning() << "unknown type " << atts.value("type");
 }
--- cd3core/saxloader.h
+++ cd3core/saxloader.h
@@ -11,22 +11,22 @@ class ISimulation;
 class SaxLoader : public QXmlDefaultHandler
 {
 public:
-    SaxLoader(IModel *m);
-    ~SaxLoader();
-    bool startElement(const QString &ns,
-                      const QString &lname,
-                      const QString &qname,
-                      const QXmlAttributes &atts);
-    bool endElement(const QString &ns,
-                    const QString &lname,
-                    const QString &qname);
-    ISimulation *load(QFile &f);
+	SaxLoader(IModel *m);
+	~SaxLoader();
+	bool startElement(const QString &ns,
+					  const QString &lname,
+					  const QString &qname,
+					  const QXmlAttributes &atts);
+	bool endElement(const QString &ns,
+					const QString &lname,
+					const QString &qname);
+	ISimulation *load(QFile &f);
 private:
-    void loadPlugin(QString path);
-    void loadParameter(const QXmlAttributes &atts);
-    SaxLoaderPriv *pd;
-    Node *current;
-    std::string source_id, source_port, sink_port, sink_id;
+	void loadPlugin(QString path);
+	void loadParameter(const QXmlAttributes &atts);
+	SaxLoaderPriv *pd;
+	Node *current;
+	std::string source_id, source_port, sink_port, sink_id;
 };
 
 #endif // SAXLOADER_H
