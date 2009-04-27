198f2db1d8be4e2a24fad7cad0ff45d28a58e861
Gregor Burger <burger.gregor@gmail.com>
2009-04-27 10:43:13 +0200

    optional use boost thread in tqueue

--- cd3core/tqueue.h
+++ cd3core/tqueue.h
@@ -1,9 +1,16 @@
 #ifndef TQUEUE_H
 #define TQUEUE_H
 
+#define CD3_BOOST_SYNC
+
 #include <queue>
+#ifdef CD3_BOOST_SYNC
+#include <boost/thread.hpp>
+using namespace boost;
+#else
 #include <QMutex>
 #include <QWaitCondition>
+#endif
 
 template <typename T>
 class tqueue
@@ -11,13 +18,35 @@ class tqueue
 public:
 	tqueue(){}
 
+#ifdef CD3_BOOST_SYNC
+	void enqueue(T t) {
+		{
+			unique_lock<boost::mutex> lock(mut);
+			q.push(t);
+		}
+		wait.notify_one();
+	}
+#else
 	void enqueue(T t) {
 		lock.lock();
 		q.push(t);
 		lock.unlock();
 		wait.wakeOne();
-	}
 
+	}
+#endif
+
+#ifdef CD3_BOOST_SYNC
+	T dequeue() {
+		unique_lock<boost::mutex> lock(mut);
+		while (q.empty()) {
+			wait.wait(lock);
+		}
+		T front = q.front();
+		q.pop();
+		return front;
+	}
+#else
 	T dequeue() {
 		lock.lock();
 		while (q.empty()) {
@@ -28,11 +57,16 @@ public:
 		lock.unlock();
 		return front;
 	}
-
+#endif
 private:
 	std::queue<T> q;
+#ifdef CD3_BOOST_SYNC
+	boost::mutex mut;
+	boost::condition_variable wait;
+#else
 	QMutex lock;
 	QWaitCondition wait;
+#endif
 };
 
 #endif // TQUEUE_H
