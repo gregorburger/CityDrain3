af26246d38d665fe9e37f543faeb983affb569ea
Gregor Burger <burger.gregor@gmail.com>
2009-05-18 12:19:50 +0200

    also run the tests

--- bench/gen-paper-imgs.sh
+++ bench/gen-paper-imgs.sh
@@ -5,7 +5,7 @@ tests="tree-2 ibk par-8-10 long-1000 long-10 par-2-10 par-8-100 tree-10"
 
 for t in $tests
 do
-#	nice ./bench/timejob.sh $t
+	nice ./bench/timejob.sh $t
 #	./bench/plotthree.sh time/$t-*-avg.txt | gnuplot > imgs/$t.eps
 	python ./bench/plot.py $t
 done 
