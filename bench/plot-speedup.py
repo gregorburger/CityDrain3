33abf991abead4db381fc83ebfb7289fb1256523
Gregor Burger <burger.gregor@gmail.com>
2009-09-16 18:36:51 +0200

    add speedup plot skript

---
+++ bench/plot-speedup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+from pylab import *
+import glob
+import sys
+
+inch=2.54
+
+if len(sys.argv) < 3:
+	print "usage: plot.py model pid"
+	exit(-1)
+
+format="png"
+	
+#if len(sys.argv) > 3:
+#	format=sys.argv[3]
+
+rc('font', size=8)
+#rc('figure.subplot', left=0.13, right=0.97, top=0.97, bottom=0.18)
+rc('figure.subplot', left=0.15, right=0.97, top=0.97, bottom=0.15)
+
+model = sys.argv[1]
+pid = sys.argv[2]
+
+sim=['ordered', 'par', 'pipe']
+appr_legend={'ordered':'OPS', 'par':'FPS', 'pipe':'PPS'}
+
+f=figure(figsize=(8/inch, 5.1/inch))
+ls = ['-', '--', ':']
+lsi = 0
+ymax=0
+lines=[]
+lines_names=[]
+
+linear_ploted = False
+
+for s in sim:
+	strat = 'time/%s-%s-%s-time-avg.txt' % (model, s, pid)
+	csv = csv2rec(strat, delimiter="\t", names=['nthreads', 'speedup'])
+	if (not linear_ploted):
+		r = range(len(csv.nthreads))
+		plot(r, r)
+	csv.speedup = map(lambda r: csv.speedup[0] / r, csv.speedup)
+	line=plot(csv.nthreads, csv.speedup)
+	line[0].set_linestyle(ls[lsi])
+	lines.append(line)
+	lines_names.append(appr_legend[s])
+	lsi+=1	
+	ymax=max(ymax, line[0].get_ydata()[0])
+
+ylim(ymax=ymax*1.2, ymin=0)
+xlabel('number of threads')#, va='bottom')
+ylabel('runtime (ms)')#, ha='left')
+l=legend(lines, lines_names, 'best')
+l.draw_frame(False)
+#savefig('imgs/%s-%s.%s' % (model, pid, format), format=format, dpi=400)#, bbox_inches="tight")
+show()
