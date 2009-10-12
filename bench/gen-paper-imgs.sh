d2834ec67b7ecc8b80a5f0ee45cc13d17ea7771d
Gregor Burger <gregor@pc53-c815.(none)>
2009-10-12 12:34:36 +0200

    update plot compare start by 1 not 0

--- bench/gen-paper-imgs.sh
+++ bench/gen-paper-imgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #tests="tree-2 ibk par-8-10 long-1000 long-10 par-2-10 par-8-100 tree-10"
-tests="ibk long-10 long-100 par-2-10 par-2-100 par-4-10 par-4-100 par-8-10 par-8-100 tree-2 tree-4 tree-7 long-1000"
+tests="ibk long-10 long-100 par-2-10 par-2-100 par-4-10 par-4-100 par-8-10 par-8-100 tree-2 tree-4 tree-7 tree-10 long-1000"
 #tests="long-1000"
 #tests="tree-2"
 
