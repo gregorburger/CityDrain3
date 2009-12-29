51df0866d931cb6a9b7c322edd0a7d1668cd0ff2
Gregor Burger <gregor@gregor-laptop.(none)>
2009-12-29 12:56:27 +0100

    cleanup fileout node

--- src/nodes/fileout.cpp
+++ src/nodes/fileout.cpp
@@ -11,40 +11,37 @@ CD3_DECLARE_NODE_NAME(FileOut)
 FileOut::FileOut()
 	: Node(), stream(&file)
 {
-	out_file_name = new std::string();
-	addParameter(ADD_PARAMETERS_P(out_file_name));
+	out_file_name = "/tmp/out.txt";
+	addParameter(ADD_PARAMETERS(out_file_name));
 	addInPort(ADD_PARAMETERS(in));
-
-	first_run = true;
 }
 
 FileOut::~FileOut() {
-	delete out_file_name;
 }
 
 void FileOut::init(int start, int stop, int dt) {
 	(void) start;
 	(void) stop;
 	(void) dt;
-	cd3assert(out_file_name, "out_file_name not defined");
-	file.setFileName(QString::fromStdString(*out_file_name));
+	cd3assert(out_file_name.size(), "out_file_name not defined");
+	file.setFileName(QString::fromStdString(out_file_name));
 	if (!file.isOpen()) {
 		file.open(QFile::WriteOnly);
 	}
-	Logger() << this << "using fileout:" << *out_file_name;
+	Logger() << this << "using fileout:" << out_file_name;
 	stream.setRealNumberPrecision(10);
 }
 
 void FileOut::deinit() {
-	//file->close();
 	if (file.isOpen())
 		file.close();
 }
 
 int FileOut::f(int time, int dt) {
-	stream << qSetFieldWidth(15);
-	stream << fixed;
+	static bool first_run = true;
 	if (first_run) {
+		stream << qSetFieldWidth(15);
+		stream << fixed;
 		stream << "time";
 		BOOST_FOREACH(std::string name, Flow::getNames()) {
 			stream << "\t" << QString::fromStdString(name);
--- src/nodes/fileout.h
+++ src/nodes/fileout.h
@@ -9,16 +9,15 @@
 CD3_DECLARE_NODE(FileOut)
 public:
 	FileOut();
-	~FileOut();
+	virtual ~FileOut();
 	virtual int f(int time, int dt);
 	virtual void init(int start, int stop, int dt);
 	virtual void deinit();
 private:
 	Flow in;
-	std::string *out_file_name;
+	string out_file_name;
 	QFile file;
 	QTextStream stream;
-	bool first_run;
 };
 
 #endif // FILEOUT_H
