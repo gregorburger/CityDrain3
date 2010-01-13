e748879a823726153c25e95bd422a78dc7116402
Gregor Burger <gregor@gregor-laptop.(none)>
2010-01-13 23:53:05 +0100

    init logger before instantiating nodes

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,10 +1,13 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <log.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+	Log::init(&cerr, Debug);
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
+	return a.exec();
 }
