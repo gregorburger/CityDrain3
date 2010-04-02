bf21c9b6008fbb09d3b078ed8426c815b7f65309
Gregor Burger <gregor.burger@uibk.ac.at>
2010-04-02 09:37:53 +0200

    full fix rainread

--- src/nodes/ixxrainread.cpp
+++ src/nodes/ixxrainread.cpp
@@ -58,7 +58,7 @@ bool IxxRainRead::init(ptime start, ptime end, int dt) {
 	}
 
 	if (first_time > start) { //fill up with zeroes
-		long zerosecs = time_period(data->current_time, start).length().total_seconds();
+		long zerosecs = time_period(start, data->current_time).length().total_seconds();
 		Logger(Debug) << "filling up rainbuffer with" << zerosecs << "seconds of zero";
 		while (zerosecs > 0) {
 			data->rain_buf.put(dt, 0.0);
@@ -75,6 +75,7 @@ void IxxRainRead::deinit() {
 }
 
 int IxxRainRead::f(ptime time, int dt) {
+	(void) time;
 	while (data->rain_buf.available() < dt) {
 		if (data->in.eof()) {
 			data->rain_buf.put(dt, 0.0);
