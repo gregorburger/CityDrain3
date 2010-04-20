0a40f262355d29f9fea92edf56a3d7d8d4c1c7c2
Gregor Burger <gregor.burger@uibk.ac.at>
2010-04-20 09:50:33 +0200

    some pump updates
    - fix pump. when the pump was on is not under Voff it stays on.
    - mix concentrations

--- src/nodes/simplepump.cpp
+++ src/nodes/simplepump.cpp
@@ -1,5 +1,6 @@
 #include "simplepump.h"
-#include <QDebug>
+#include <flowfuns.h>
+#include <boost/assign.hpp>
 
 CD3_DECLARE_NODE_NAME(SimplePump)
 
@@ -20,16 +21,30 @@ SimplePump::SimplePump() {
 		.setUnit("m^3/s");
 	addParameter(ADD_PARAMETERS(Vmax))
 		.setUnit("m^3");
+	addState(ADD_PARAMETERS(on));
 }
 
 SimplePump::~SimplePump() {
 
 }
 
+using namespace boost::assign;
+
 int SimplePump::f(ptime time, int dt) {
 	(void) time;
-	volume[0] += in[0] * dt;
-	if (volume[0] > Von) {
+
+	//volume[0] += in[0] * dt;
+	vector<Flow> tomix;
+	Flow inq = in;
+	inq[0] *= dt;
+	tomix += volume, inq;
+	volume = FlowFuns::mix(tomix);
+	if (volume[0] > Von || on) {
+		on = true;
+	}
+	out_p = volume; //assign concentrations
+	out_w = volume;
+	if (on) {
 		double volume_after_pump = max(Voff, volume[0] - Qpp);
 		out_p[0] = (volume[0] - volume_after_pump)/dt;
 		volume[0] = volume_after_pump;
@@ -38,6 +53,9 @@ int SimplePump::f(ptime time, int dt) {
 	} else {
 		out_p[0] = 0.0;
 		out_w[0] = 0.0;
+	}
+	if (volume[0] <= Voff) {
+		on = false;
 	}
 	return dt;
 }
@@ -54,6 +72,7 @@ bool SimplePump::init(ptime start, ptime end, int dt) {
 	}
 	volume.clear();
 	Qpp = Qp * dt;
+	on = false;
 	return true;
 }
 
