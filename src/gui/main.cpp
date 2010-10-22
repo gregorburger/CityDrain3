5afa28230ce5acb213a34dade9bf278d12f59917
Gregor Burger <burger.gregor@gmail.com>
2010-10-22 10:12:42 +0000

    also add global path to cd3-gui main

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,8 +1,11 @@
 #include <QtGui/QApplication>
+#include <QFileInfo>
+#include <QDir>
 #include "mainwindow.h"
 #include <log.h>
 #include <logsink.h>
 #include <iostream>
+#include <noderegistry.h>
 
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
@@ -10,7 +13,12 @@ int main(int argc, char *argv[]) {
 	QApplication::setOrganizationName("University of Innsbruck - "
 								"Unit of Environmental Engineering");
 	QApplication::setOrganizationDomain("http://www.uibk.ac.at/umwelttechnik/");
+#ifndef PYTHON_DISABLED
+	QFileInfo me(argv[0]);
+	NodeRegistry::addToPythonPath(me.dir().path().toStdString());
+#endif
 	MainWindow w;
 	w.show();
+
 	return a.exec();
 }
