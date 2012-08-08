151f4ad634975d3adf7ba3540b24962bd50634ec
Michael Mair <michael.mair@uibk.ac.at>
2012-08-08 12:10:45 +0200

    replaced: string "node" -> "simulation" in log output

--- src/cd3core/simulationregistry.cpp
+++ src/cd3core/simulationregistry.cpp
@@ -52,7 +52,7 @@ void SimulationRegistry::addNativePlugin(const std::string &plugin_path) {
 	if (regSimFun) {
 		regSimFun(this);
 	} else {
-		Logger(Warning) << plugin_path << " has no node register hook";
+        Logger(Warning) << plugin_path << " has no simulation register hook";
 	}
 }
 
